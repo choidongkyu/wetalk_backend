d80851537cce96a2ec9c608aaf450805f006978e
dkchoi <ehdqkwk92@gmail.com>
2021-04-29 19:08:18 +0900

    중복체크 로직 추가

--- signup.php
+++ signup.php
@@ -1,11 +1,25 @@
 <?php 
-    //id 데이터가 정상적으로 올 경우 pw, email 데이터가 정상적으로 왔다고 가정하고 따로 데이터가 있는지 검증 하지 않음
     if(isset($_POST["id"])) {
         $id = $_POST["id"];
 
         //mysql 연결
         $db = mysqli_connect("127.0.0.1", "root", "Ddr7979556!", "wetalk");
         if($db) {
+            
+            //중복체크
+            $sql = "select id from t_user";
+            $res = mysqli_query($db, $sql);
+            if (mysqli_num_rows($res) > 0) {
+                while($row = mysqli_fetch_assoc($res)) {
+                    //중복되는 id가 있을 경우
+                    if($row['id'] == $id) {
+                        $result = -100;
+                        mysqli_close($db);
+                        exit;
+                    }
+                }
+            }
+
              $sql = "insert into t_user (id) values ('$id')";
              $res = $db->query($sql);
              //정상적으로 데이터베이스가 생성 될 경우
