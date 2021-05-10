d96ec5fe248ba3d5bf58ccf46fa3e04900de99ee
dkchoi <ehdqkwk@naver.com>
2021-05-10 17:44:26 +0900

    fix

--- signup.php
+++ signup.php
@@ -66,13 +66,13 @@
 
     if(isset($_GET["request"])) {
         if($_GET["request"] === "getName") { //클라이언트가 이름을 요청한 경우
-            $id = '+'.$_GET["phone"];
+            $id = $_GET["phone"];
             $db = mysqli_connect("127.0.0.1", "root", "Ddr7979556!", "wetalk");
             if($db) {
                 $sql = "select name from t_user where id = '$id'";
                 $res = mysqli_query($db, $sql);
                 while($row = mysqli_fetch_assoc($res)) {
-                    echo $row;
+                    echo $row["name"];
                 }
             }
             mysqli_close($db);
