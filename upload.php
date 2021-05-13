d78551708c1fa18f5876d7303f3a0e6aa6169728
dkchoi <ehdqkwk92@gmail.com>
2021-05-13 23:41:01 +0900

    디버깅코드 추가

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
         $id = $_POST['id'];
         $result = 0;
         $file_path = './profile_image/'.$id.'.jpg';
-        if(is_uploaded_file($_FILES['file']['tmp_name'])) {
+        if(file_exists($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
             $result = 1;
             if(move_uploaded_file($_FILES['file']['tmp_name'], $file_path)) {
                 $result = 200;
