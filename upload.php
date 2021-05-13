b90b753ac6ffccfee205f85a759a77b8422a2b16
dkchoi <ehdqkwk92@gmail.com>
2021-05-13 23:32:46 +0900

    디버깅 코드 추가

--- upload.php
+++ upload.php
@@ -1,11 +1,14 @@
 <?php 
     if(isset($_POST['id'])) {
         $id = $_POST['id'];
-        $result = -1;
+        $result = 0;
         $file_path = './profile_image/'.$id.'.jpg';
         if(is_uploaded_file($_FILES['file']['tmp_name'])) {
+            $result = 1;
             if(move_uploaded_file($_FILES['file']['tmp_name'], $file_path)) {
                 $result = 200;
+            }else {
+                $result = -1;
             }
         }
         echo $result;
