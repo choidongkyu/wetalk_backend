05a0c5828b4db0d495afc5c794c78751426f6d18
dkchoi <ehdqkwk92@gmail.com>
2021-07-12 00:00:43 +0900

    test

--- index.php
+++ index.php
@@ -6,27 +6,33 @@
 <body>
  
 <?php
-phpinfo();
-echo "MySql 연결 테스트<br>";
+// phpinfo();
+// echo "MySql 연결 테스트<br>";
  
-$db = mysqli_connect("127.0.0.1", "root", "Ddr7979556!", "mysql");
+// $db = mysqli_connect("127.0.0.1", "root", "Ddr7979556!", "mysql");
  
-if($db){
-    echo "connect : 성공<br>";
+// if($db){
+//     echo "connect : 성공<br>";
+// }
+// else{
+//   echo "Error: Unable to connect to MySQL." . PHP_EOL;
+
+// 	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+
+// 	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+
+// 	exit();
+// }
+ 
+// $result = mysqli_query($db, 'SELECT VERSION() as VERSION');
+// $data = mysqli_fetch_assoc($result);
+// echo $data['VERSION'];
+
+$path = "+821026595819,+15555215558";
+if (!file_exists($path)) {
+    mkdir($path, 0777, true);
 }
-else{
-  echo "Error: Unable to connect to MySQL." . PHP_EOL;
 
-	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-
-	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-
-	exit();
-}
- 
-$result = mysqli_query($db, 'SELECT VERSION() as VERSION');
-$data = mysqli_fetch_assoc($result);
-echo $data['VERSION'];
 ?>
  
 </body>
